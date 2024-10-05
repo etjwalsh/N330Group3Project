dfbe440358492d62faae94e40d4b102478afcc9a
Electro13 <seanmackey169@gmail.com>
2024-10-05 03:20:12 -0400

    Wideshot and Laser :)

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,8 +1,23 @@
 extends CharacterBody3D
 
+var weaponNum = 3
+#0: spreadshot
+#1: laser
+#2: sword
+var weapon = 0
+
+var bulletSpawn = .516
+var wideshotBuffer = 0
+var weaponRotation = 0
+var bulletIns
+var laserIns
 
 const SPEED = 10.0
+const wideshotAngle = PI/8
+const wideshotBuffMax = .08
 
+const bullet = preload("res://Scenes/Bullets/PlayerBullet/player_bullet.tscn")
+const laser = preload("res://Scenes/Bullets/PlayerLaser/player_laser.tscn")
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -19,5 +34,37 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
 	move_and_slide()
+
+func _process(delta: float) -> void:
+	if(Input.is_action_pressed("use")):
+		match(weapon):
+			0:
+				if(wideshotBuffer == 0):
+					bulletIns = bullet.instantiate()
+					bulletIns.rotation.y = weaponRotation + (wideshotAngle * (bulletSpawn - .5))
+					bulletIns.position = position
+					bulletSpawn += .618
+					if(bulletSpawn > 1):
+						bulletSpawn -= 1
+					get_parent().add_child(bulletIns)
+				wideshotBuffer += 1 * delta
+				if(wideshotBuffer >= wideshotBuffMax):
+					wideshotBuffer = 0
+			1:
+				if(!is_instance_valid(laserIns)):
+					laserIns = laser.instantiate()
+					add_child(laserIns)
+				laserIns.rotation.y = weaponRotation
+			2:
+				pass
+	if(is_instance_valid(laserIns) && (!Input.is_action_pressed("use") || weapon != 1)):
+		laserIns.queue_free()
+	if(Input.is_action_just_pressed("switch")):
+		weapon += 1
+		if(weapon >= weaponNum):
+			weapon = 0
+	
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouse:
+		weaponRotation = -Vector2(960, 540).angle_to_point(event.position) + PI/2
