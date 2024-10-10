9295fb03a4608c4df2045186677ca65ae61b9b35
Electro13 <seanmackey169@gmail.com>
2024-10-10 15:08:20 -0400

    Weapon Hitboxes

--- Scenes/Bullets/PlayerLaser/player_laser.gd
+++ Scenes/Bullets/PlayerLaser/player_laser.gd
@@ -1,10 +1,12 @@
 extends Sprite3D
 
+@export var shape:CollisionShape3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	position.z = 16
 	scale.y = 100
+	shape.scale.y = 100
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
