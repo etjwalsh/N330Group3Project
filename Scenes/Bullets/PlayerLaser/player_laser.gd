b619abe9e5c44f8d0669c5477db5969495f33a84
Electro13 <seanmackey169@gmail.com>
2024-10-10 17:40:56 -0400

    laser collision

--- Scenes/Bullets/PlayerLaser/player_laser.gd
+++ Scenes/Bullets/PlayerLaser/player_laser.gd
@@ -1,14 +1,19 @@
-extends Sprite3D
-
-@export var shape:CollisionShape3D
+extends RigidBody3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	position.z = 16
-	scale.y = 100
-	shape.scale.y = 100
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
-	pass
+	if($RayCast3D.is_colliding()):
+		print(global_position)
+		$Sprite3D.scale.y = global_position.distance_to($RayCast3D.get_collision_point()) * 3
+		$CollisionShape3D.scale.y = global_position.distance_to($RayCast3D.get_collision_point()) * 3
+		$Sprite3D.position.z = global_position.distance_to($RayCast3D.get_collision_point()) * 48 / 100
+		$CollisionShape3D.position.z = global_position.distance_to($RayCast3D.get_collision_point()) * 48 / 100
+	else:
+		$Sprite3D.scale.y = 100
+		$CollisionShape3D.scale.y = 100
+		$Sprite3D.position.z = 16
+		$CollisionShape3D.position.z = 16
