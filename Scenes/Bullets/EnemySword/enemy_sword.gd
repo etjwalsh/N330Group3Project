a96b0643b0fa6192ab43fed0b8941c76b9b7a76f
Electro13 <seanmackey169@gmail.com>
2024-11-14 20:00:39 -0500

    player health update

--- Scenes/Bullets/EnemySword/enemy_sword.gd
+++ Scenes/Bullets/EnemySword/enemy_sword.gd
@@ -1,6 +1,7 @@
 extends Area3D
 
 var time = .5
+var hit = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,3 +13,9 @@ func _process(delta: float) -> void:
 	time -= delta
 	if(time <= 0):
 		queue_free()
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if("health" in body && !hit):
+		body.health -= 1
+		hit = true
--- Scenes/UI/ui.gd
+++ Scenes/UI/ui.gd
@@ -1,11 +1,40 @@
 extends Control
 
+const heart = preload("res://Images/helf.png")
+
+var heartcount = 5
+var hearts = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	var unitSize = get_viewport().get_visible_rect().size.x / 20
+	for i in heartcount:
+		hearts.append(Sprite2D.new())
+		hearts[-1].position = Vector2(i * unitSize * 1.1, 0)
+		hearts[-1].scale = Vector2(unitSize / 16, unitSize / 16)
+		hearts[-1].texture = heart
+		hearts[-1].centered = false
+		add_child(hearts[-1])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	$Energy.value = PlayerAutoload.energy
+	
+	if(PlayerAutoload.health >= 0):
+		if(hearts.size() > PlayerAutoload.health):
+			for i in hearts.size() - PlayerAutoload.health:
+				hearts[-1].queue_free()
+			hearts.resize(PlayerAutoload.health)
+	else:
+		PlayerAutoload.health = 0
+
+
+func _on_resized() -> void:
+	var unitSize = get_viewport().get_visible_rect().size.x / 20
+	for i in hearts.size():
+		hearts[i].position = Vector2(i * unitSize * 1.1, 0)
+		hearts[i].scale = Vector2(unitSize / 16, unitSize / 16)
+	
+	$Energy.size = get_viewport().get_visible_rect().size / Vector2(40, 2)
+	$Energy.position = Vector2(get_viewport().get_visible_rect().size.x - $Energy.size.x, 0)
