df6b0e41df8bf1d3b10d65eaff78d0d563530c24
Walsh, Ethan Joseph <ethan.walsh282003@gmail.com>
2024-12-14 21:02:02 -0500

    doors and spawners and pits
    doors open/close, pits dont eat bullets, and spawners spawn a little slower

--- Scenes/Enemies/enemySpawner/enemy_spawner.gd
+++ Scenes/Enemies/enemySpawner/enemy_spawner.gd
@@ -4,12 +4,28 @@ var wideshotEnemy = preload("res://Scenes/Enemies/RangedEnemy/rangedEnemy.tscn")
 var shotgunEnemy = preload("res://Scenes/Enemies/RangedEnemy2/ranged_enemy_2.tscn")
 var staticEnemy = preload("res://Scenes/Enemies/RangedEnemyStatic/ranged_enemy_static.tscn")
 var meleeEnemy = preload("res://Scenes/Enemies/MeleeEnemy/MeleeEnemy.tscn")
+var door = preload("res://Scenes/rooms/roomDoor.tscn")
 
 var enemyToSpawn
 @export var maxEnemies = 5 # export so we can change the amount of enemies spawned per instance of the object
+var enemiesSpawnNum = maxEnemies
 var spawnPosX 
 var spawnPosY
 var spawnPosZ 
+var numEnemies = 0
+var enemiesArr = []
+var enemiesKilled = 0
+
+
+func _ready():
+	$BigWall.visible = false
+	$BigWall.position = Vector3(-10,-10,-10)
+	
+func _process(delta: float) -> void:
+	_open_doors(enemiesArr)
+	if enemiesKilled == maxEnemies:
+		$BigWall.visible = false
+		$BigWall.position = Vector3(-10,-10,-10)
 
 func _spawnEnemy():
 	var enemyIndex = int(randi_range(0,3))
@@ -29,12 +45,13 @@ func _spawnEnemy():
 	spawnPosZ = randf_range($Area3D.position.y - 10, $Area3D.position.y + 10)
 	
 	var enemyToSpawnIns = enemyToSpawn.instantiate()
+	numEnemies += 1
 	enemyToSpawnIns.position = position + Vector3(spawnPosX, spawnPosY, spawnPosZ)
 	
-	if maxEnemies > 0:
+	if enemiesSpawnNum > 0:
 		get_parent().add_child(enemyToSpawnIns)
-		maxEnemies -= 1
-
+		enemiesArr.append(enemyToSpawnIns)
+		enemiesSpawnNum -= 1
 
 func _on_spawn_delay_timeout() -> void:
 	_spawnEnemy()
@@ -44,3 +61,12 @@ func _on_spawn_delay_timeout() -> void:
 func _on_area_3d_body_entered(body: Node3D) -> void:
 	if (body.name == "Player"):
 		$spawnDelay.start()
+		#close the doors
+		$BigWall.position = Vector3(0,0,0)
+		$BigWall.visible = true
+
+func _open_doors(array):
+	enemiesKilled = 0
+	for i in array.size():
+		if not is_instance_valid(array[i]):
+			enemiesKilled += 1
